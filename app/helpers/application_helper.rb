68a540e5be8c356cae22d19eba5ee3fe57137dcd
Joanie Davis <joanie.e.davis1@gmail.com>
2017-06-16 09:21:09 -0400

    Chapter 5 Exercises
    A bit more functionality in terms of links added, as well as updating
    some tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
-    else 
+    else
       page_title + " | " + base_title
-    end 
+    end
   end
 end
