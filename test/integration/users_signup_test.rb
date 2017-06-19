4d705981e444c6a1e5c70dd243091121f7c53743
Joanie Davis <joanie.e.davis1@gmail.com>
2017-06-19 09:22:24 -0400

    Chapter 7 Exercises continued, add flash confirmation of user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: {user: {name: "Example User",
+                                       email: "user@example.com",
+                                       password: "password",
+                                       password: "password" } }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 end
