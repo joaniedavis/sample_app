1943460bcb9d248d61a63268cab6ce7e1009b37d
Joanie Davis <joanie.e.davis1@gmail.com>
2017-07-14 13:17:01 -0400

    Adds image search feature
    Adds a link and routes and functionality for pages searching through
    Unsplash images.

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,10 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Unsplash.configure do |config|
+  config.application_id     = Figaro.env.application_id
+  config.application_secret = Figaro.env.application_secret
+  config.application_redirect_uri = "/image_search/search"
+  config.utm_source = "alices_terrific_client_app"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'image_search/search'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   get  '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/image_search', to: 'image_search#show'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get  '/login', to: 'sessions#new'
